bbed0c289c31a2f19da1ea82d660a428009419b6
troversones <kisbencerobert2002@gmail.com>
2026-03-15 22:03:54 +0100

    Operators done

--- PythonBase/Operators.py
+++ PythonBase/Operators.py
@@ -42,3 +42,47 @@ print(x) # and magically it turns into 01100 = 12
 
 print(" \"Walrus\" operator := which basically it assigns value and returns at the same time")
 print(x := 3)
+
+print("comparison operators are just very basic so I'm skipping those I know anyway")
+
+print("Logical operators: 'and' 'or' 'not' which basically is &,| or ! respectively")
+
+print("Identity op: (is , is not) very simple ye?")
+'''
+Membership op: in, not in
+'''
+
+print("Bitwise operators:")
+'''
+& - 'AND' : Sets each bit to 1 if both bits are 1
+| - 'OR' : Sets each bit to 1 if one of two bits is 1
+^ - 'XOR' : Sets each bit to 1 if only one of two bits is 1
+~ - 'NOT' : Inverts all the bits
+<< - 'Zero fill left shift' : Shift left by pushing zeros in from the right and let the leftmost bits fall off
+>> - 'Signed right shift' : Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
+'''
+
+print(6 & 3) #binaries: 0110 and 0011
+#which prints out 2 since it becomes 0010
+
+print(6 | 3) #this prints out 7 -> 0111
+
+print(6 ^ 3) #this prints 5 -> 0101
+#if both is 1 or 0 then it sets to 0
+
+'''
+might be worth to mention the Precendence order (top-bottom descending order by priority)
+()	Parentheses	
+**	Exponentiation	
++x  -x  ~x	Unary plus, unary minus, and bitwise NOT	
+*  /  //  %	Multiplication, division, floor division, and modulus	
++  -	Addition and subtraction	
+<<  >>	Bitwise left and right shifts	
+&	Bitwise AND	
+^	Bitwise XOR	
+|	Bitwise OR	
+==  !=  >  >=  <  <=  is  is not  in  not in 	Comparisons, identity, and membership operators	
+not	Logical NOT	
+and	AND	
+or	OR
+'''
